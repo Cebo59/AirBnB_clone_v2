183782bd2861466e1b2914f3897e247386aef196
Cebo59 <cebokuhledhlamini59@gmail.com>
2023-05-06 21:39:50 +0000

    Files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,57 +1,45 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers."""
+""""Fabric script that distributes an archive to web servers"""
 
-from os.path import isfile
-from fabric.api import env, put, run
-from datetime import datetime
+from fabric.api import *
+import os
 
-env.hosts = ['54.165.197.124', '54.234.35.46']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ['3.235.198.120', '3.239.50.204']
 
-def do_pack():
-    """ Generates a .tgz archive """
-    
-    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "web_static_{}.tgz".format(time_now)
-    archive_path = "versions/{}".format(filename)
-
-    print("Packing web_static to {}".format(archive_path))
-
-    local("mkdir -p versions")
-    result = local("tar -cvzf {} web_static".format(archive_path))
-
-    if result.failed:
-        return None
-    
-    print("Successfully packed web_static to {}".format(archive_path))
-    
-    return archive_path
 
 def do_deploy(archive_path):
-    """ Distributes an archive to webserver """
-    
-    if not isfile(archive_path):
-        return False
-    
-    print("Deploying new version")
-    
-    archive_name = archive_path.split("/")[-1]
-    folder_name = archive_name[: -4]
-    dir_path = "/data/web_static/releases/{}".format(folder_name)
-    
-    put(archive_path, "/tmp/")
-    run("mkdir -p {}".format(dir_path))
-    result = run("tar -xzf /tmp/{} -C {}".format(archive_name, dir_path))
-    
-    if result.failed:
-        return False
-    
-    run("cp -r {}/web_static/* {}".format(dir_path, dir_path))
-    run("rm -rf /tmp/{} {}/web_static".format(archive_name, dir_path))
-    run("rm -rf /data/web_static/current")
-    run("ln -s {} /data/web_static/current".format(dir_path))
+    """Archive distributor"""
+    try:
+        try:
+            if os.path.exists(archive_path):
+                arc_tgz = archive_path.split("/")
+                arg_save = arc_tgz[1]
+                arc_tgz = arc_tgz[1].split('.')
+                arc_tgz = arc_tgz[0]
 
-    print("New version deployed!")
+                """Upload archive to the server"""
+                put(archive_path, '/tmp')
 
-    return True
+                """Save folder paths in variables"""
+                uncomp_fold = '/data/web_static/releases/{}'.format(arc_tgz)
+                tmp_location = '/tmp/{}'.format(arg_save)
+
+                """Run remote commands on the server"""
+                run('mkdir -p {}'.format(uncomp_fold))
+                run('tar -xvzf {} -C {}'.format(tmp_location, uncomp_fold))
+                run('rm {}'.format(tmp_location))
+                run('mv {}/web_static/* {}'.format(uncomp_fold, uncomp_fold))
+                run('rm -rf {}/web_static'.format(uncomp_fold))
+                run('rm -rf /data/web_static/current')
+                run('ln -sf {} /data/web_static/current'.format(uncomp_fold))
+                run('sudo service nginx restart')
+                return True
+            else:
+                print('File does not exist')
+                return False
+        except Exception as err:
+            print(err)
+            return False
+    except Exception:
+        print('Error')
+        return False
